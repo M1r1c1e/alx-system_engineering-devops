73e341ca02ec25039455ad1b6f392cc931b768b6
M1r1c1e <andersonmiracle111@.com>
2024-01-30 08:51:12 -0800

    regular expression

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,25 +1,4 @@
 #!/usr/bin/env ruby
+# This script searches log files and outputs the [SENDER],[RECEIVER],[FLAGS]
 
-# Checking if input CSV file is provided as an argument
-unless ARGV.length == 1
-  puts "Usage: #{$PROGRAM_NAME} <input_csv_file>"
-  exit 1
-end
-
-# Input the CSV file
-input_file = ARGV[0]
-
-# Checking if the input file exists
-unless File.exist?(input_file)
-  puts "Error: Input file '#{input_file}' not found."
-  exit 1
-end
-
-# Output header
-puts "Sender,Receiver,Flags"
-
-# Process CSV file and retreive relevant information
-File.foreach(input_file) do |line|
-  sender, receiver, flags = line.chomp.split(',')
-  puts "#{sender},#{receiver},#{flags}"
-end
+puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(",")
