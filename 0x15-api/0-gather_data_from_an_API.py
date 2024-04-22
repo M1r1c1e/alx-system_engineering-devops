8ead0f2c5ffbc1035f626beb16d16b6589f92d04
M1r1c1e <andersonmiracle111@.com>
2024-04-22 14:10:24 -0700

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,18 +7,10 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     name = requests.get("{}users/{}".format(url, sys.argv[1])).json()
     tasks = requests.get("{}users/{}/todos".format(url, sys.argv[1])).json()
-    com_tasks = []
-    for task in tasks:
-        if task.get("completed") is True:
-            com_tasks.append(task)
-    titles = []
-    for task in tasks:
-        if task.get("completed") is True:
-            titles.append(task.get("title"))
+    com_tasks = [task for task in tasks if task.get("completed") is True]
+    titles = [task.get("title") for task in tasks
+              if task.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):"
-          .format(name.get("name"), len(c_tasks), len(tasks)))
+          .format(name.get("name"), len(com_tasks), len(tasks)))
     for title in titles:
         print("\t {}".format(title))
-
-
-
