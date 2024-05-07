da2e04c2be4d676950c3fda8f65ac85b394d6741
M1r1c1e <andersonmiracle111@.com>
2024-05-07 11:02:20 -0700

    advance api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-"""Querying the Reddit API and prints the titles of the 
-first 10 hot posts listed for a given subreddit
-"""
+"""Querying subreddit"""
+
 import requests
 
 
 def top_ten(subreddit):
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    header = {"User-Agent": "Mozilla/5.0"}
+    """Query Reddit and print titles of the first 10 hot posts"""
 
-    data = requests.get(url, headers=header, allow_redirects=False)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
 
-    if data.status_code == 200:
-        data = data.json().get("data").get("children")
-        for item in data:
-            print(item.get("data").get("title"))
-    else:
-        print(None)
+    # Set a custom User-Agent to avoid too many requests error
+    headers = {'User-Agent': 'My user Agent 1.0'}
+
+    # send a GET request to the Reddit API
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    try:
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            children = response.json().get('data').get('children')
+            for i in range(10):
+                print(children[i].get('data').get('title'))
+        else:
+            print("None")
+    except Exception:
+        print("None")
