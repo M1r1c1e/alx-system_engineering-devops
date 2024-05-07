ee0389b14c714d4b9e3719e16c283e3215c5c3f3
M1r1c1e <andersonmiracle111@.com>
2024-05-07 11:48:39 -0700

    advance api

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""a recursive function that queries the Reddit API, parses the title of all hot articles, 
-and prints a sorted count of given keywords (case-insensitive, delimited by spaces
+"""Function to count words in all hot posts of a given Reddit subreddit
+   that are found in a list
 """
 import requests
 
