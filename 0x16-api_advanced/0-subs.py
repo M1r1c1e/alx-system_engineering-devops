76964d0f28f6d18527af185652e2649a56372221
M1r1c1e <andersonmiracle111@.com>
2024-05-07 10:39:34 -0700

    advance api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""querying the Reddit API and 
-returns the number of subscribers
-"""
+"""Quering Reddit"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """query a subreddit and retrive no of subscribers"""
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    headers = {'User-Agent': 'My user Agent 1.0'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        data = response.json().get('data', {})
+        sub_count = data.get('subscribers', 0)
+        return sub_count
     else:
         return 0
